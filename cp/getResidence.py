7475bffdca1e2530ea0f070d766a4e4e99546b32
Hieu Nguyen <nth.itman@gmail.com>
2016-12-12 15:06:59 +0200

    Change the residency computation to monthly basis

--- cp/getResidence.py
+++ cp/getResidence.py
@@ -9,13 +9,13 @@ import gzip
 import os
 
 prefix = ''
-filename = 'ALL_TWEETS_lat_long_username_date_country_quarter.gz'
+filename = 'ALL_TWEETS_lat_long_username_date_country_month.gz'
 filepath = os.path.join(prefix, filename)
 fname, fext = os.path.splitext(filepath)
 assert os.path.isfile(filepath) and fext == '.gz'
 with gzip.open(filepath, 'rb') as f:
     last_username = ''
-    last_quarter = ''
+    last_timeval = ''
     country_counter = {}
     for line in f:
         line = line.decode('utf-8').strip()
@@ -23,22 +23,22 @@ with gzip.open(filepath, 'rb') as f:
         try:
             username = elem[2]
             country = elem[4]
-            quarter = elem[5]
+            timeval = elem[5]
             if username == last_username:
-                if quarter == last_quarter:
+                if timeval == last_timeval:
                     if country_counter.has_key(country):
                         country_counter[country] += 1
                     else:
                         country_counter[country] = 1
                 else:
                     residence_country, _ = max(country_counter.iteritems(), key=lambda x:x[1])
-                    print username+'\t'+last_quarter+'\t'+residence_country
+                    print username+'\t'+last_timeval+'\t'+residence_country
                     country_counter ={country:1}
             else:
                 country_counter = {country:1}
                         
             last_username = username
-            last_quarter = quarter
+            last_timeval = timeval
         except Exception as e:
             pass
         
