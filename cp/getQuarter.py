73166bf6b5a7582487af82beb271fa1e8093718e
Hieu Nguyen <nth.itman@gmail.com>
2016-12-12 07:15:08 +0200

    Add option to output year-month

--- cp/getQuarter.py
+++ cp/getQuarter.py
@@ -10,11 +10,15 @@ import os
 
 quarter_dict = {'Jan':'01', 'Feb':'01', 'Mar':'01', 'Apr':'02', 'May':'02', 'Jun':'02',
               'Jul':'03', 'Aug':'03', 'Sep':'03', 'Oct':'04', 'Nov':'04', 'Dec':'04'}
+              
+month_dict = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06',
+              'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
 
 prefix = ''
 filename = 'ALL_TWEETS_lat_long_username_date_country.gz'
 filepath = os.path.join(prefix, filename)
 fname, fext = os.path.splitext(filepath)
+time_level = 2 # 1: quarter level,  2: month level
 assert os.path.isfile(filepath) and fext == '.gz'
 with gzip.open(filepath, 'rb') as f:
     for line in f:
@@ -24,7 +28,10 @@ with gzip.open(filepath, 'rb') as f:
             tweet_time_elem = elem[3].split()
             tweet_month = tweet_time_elem[1]
             tweet_year = tweet_time_elem[-1]
-            quarter = tweet_year+quarter_dict[tweet_month]
-            print quarter
+            if time_level == 1:
+                timeval = tweet_year+quarter_dict[tweet_month]
+            else:
+                timeval = tweet_year+month_dict[tweet_month]
+            print timeval
         except Exception:
             print '0'
